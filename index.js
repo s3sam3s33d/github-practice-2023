c13a928d8a54c752eadd4b66b46f9e4b7ff663fa
gavinlai999 <136124137+gavinlai999@users.noreply.github.com>
2023-09-07 16:02:16 -0700

    from biggavin

---
+++ index.js
@@ -0,0 +1,10 @@
+const gavinFunction = () =>{
+  const userQuestion = 'Do you like me? ';
+  const input = prompt(userQuestion);
+  const answer= `Why ${input}??`;
+  console.log(answer);
+};
+
+gavinFunction();
+
+
